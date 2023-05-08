b7ae869364b6c64274449211d2927beb6cdac1a0
gokul <gokulchandan18@gmail.com>
2023-05-08 16:04:57 +0530

    a

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import HamburgerMenu from "./components/HamburgerMenu";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Appheader from "./components/Appheader";
+import ShowBookDetails from "./components/ShowBookDetails";
+import UpdateBookInfo from "./components/UpdateBookInfo";
+import CreateBook from "./components/CreateBook";
+import ShowBookList from "./components/ShowBookList";
 
 class App extends Component {
   render() {
@@ -32,6 +36,10 @@ class App extends Component {
               <Route path="/register" element={<Register />}></Route>
               <Route path="/reduxhome" element={<ReduxHome />} />
               <Route path="/cart" element={<Cart />} />
+              <Route exact path="/home2" element={<ShowBookList />} />
+              <Route path="/create-book" element={<CreateBook />} />
+              <Route path="/edit-book/:id" element={<UpdateBookInfo />} />
+              <Route path="/show-book/:id" element={<ShowBookDetails />} />
             </Routes>
           </div>
         </BrowserRouter>
