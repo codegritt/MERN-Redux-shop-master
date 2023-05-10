02efd03706cbb02f9b696c4725a87b7b27316c3b
gokul <gokulchandan18@gmail.com>
2023-05-10 18:04:27 +0530

    a

--- src/App.js
+++ src/App.js
@@ -15,37 +15,36 @@ import UpdateBookInfo from "./components/UpdateBookInfo";
 import CreateBook from "./components/CreateBook";
 import ShowBookList from "./components/ShowBookList";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <BrowserRouter>
-          <Appheader />
-          <div className="App">
-            <div className={AppHome.appHome}>
-              <Header1 />
+export const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Appheader />
+        <div className="App">
+          <div className={AppHome.appHome}>
+            <Header1 />
 
-              <Header2 />
-              <HamburgerMenu />
-            </div>
-
-            {/* <Navbar /> */}
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/login" element={<Login />}></Route>
-              <Route path="/register" element={<Register />}></Route>
-              <Route path="/reduxhome" element={<ReduxHome />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route exact path="/home2" element={<ShowBookList />} />
-              <Route path="/create-book" element={<CreateBook />} />
-              <Route path="/edit-book/:id" element={<UpdateBookInfo />} />
-              <Route path="/show-book/:id" element={<ShowBookDetails />} />
-            </Routes>
+            <Header2 />
+            <HamburgerMenu />
           </div>
-        </BrowserRouter>
-      </>
-    );
-  }
-}
+
+          {/* <Navbar /> */}
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/reduxhome" element={<ReduxHome />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route exact path="/home2" element={<ShowBookList />} />
+            <Route path="/create-book" element={<CreateBook />} />
+            <Route path="/edit-book/:id" element={<UpdateBookInfo />} />
+            <Route path="/show-book/:id" element={<ShowBookDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      {/* <AnimatedRoutes /> */}
+    </>
+  );
+};
 
 export default App;
