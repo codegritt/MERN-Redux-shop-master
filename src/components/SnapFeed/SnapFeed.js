22ec8004f8eeca240bbb30ad87c4d11694a0efc7
gokul <gokulchandan18@gmail.com>
2023-05-11 18:07:27 +0530

    a

--- src/components/SnapFeed/SnapFeed.js
+++ src/components/SnapFeed/SnapFeed.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PostStatus from "./PostStatus";
+import Sidebar from "../Sidebar";
 
 function SnapFeed() {
   return (
     <div>
+      <Sidebar />
       <PostStatus />
     </div>
   );
