c0e9663aa59cad4b0eb89732cb244c1898ee3a6f
gokul <gokulchandan18@gmail.com>
2023-05-20 16:33:59 +0530

    a

--- src/components/Redux/CheckoutPage.js
+++ src/components/Redux/CheckoutPage.js
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Breadcrumbsmain from "./CheckoutPage.module.css";
+import Breadcrumbscon from "./CheckoutPage.module.css";
 
-const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+class CheckoutPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { expanded: false };
+  }
+
+  handleChange = (panel) => (event, expanded) => {
+    if (expanded) {
+      this.setState({ expanded: panel });
+    } else {
+      this.setState({ expanded: false });
+    }
+  };
+
+  handleChecked = (e) => {
+    if (e.target.checked) {
+      this.props.addShipping();
+    } else {
+      this.props.substractShipping();
+    }
+  };
+  render() {
+    return (
+      <div className={Breadcrumbscon.breadcrumbscon}>
+        <h6>Checkout Page</h6>
+
+        <div>
+          <Link to="/">
+            <span className={Breadcrumbsmain.breadcrumbsmain}>Home -></span>
+          </Link>
+          <Link to="/cart">
+            <span className={Breadcrumbsmain.breadcrumbsmain}>Cart -></span>
+          </Link>
+          <Link to="/checkout">
+            <span className={Breadcrumbsmain.breadcrumbsmain}>Checkout</span>
+          </Link>
+        </div>
+        <div className="collection">
+          <li className="collection-item">
+            <label>
+              <input
+                type="checkbox"
+                ref="shipping"
+                onChange={this.handleChecked}
+              />
+              <span>Shipping(+6$)</span>
+            </label>
+          </li>
+          <li className="collection-item">
+            <b>Total: {this.props.total} $</b>
+          </li>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    addedItems: state.addedItems,
+    total: state.total,
+  };
 };
 
-export default CheckoutPage;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addShipping: () => {
+      dispatch({ type: "ADD_SHIPPING" });
+    },
+    substractShipping: () => {
+      dispatch({ type: "SUB_SHIPPING" });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutPage);
